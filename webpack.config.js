4d058607a0966327e07c796489214245df6590cf
Sam Derbyshire <samderbyshire@sams-mbp.home>
2024-04-04 10:38:03 +0100

    Setting up tailwind and webpack, adjusted dev command to use local repo

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+  entry: {
+		index: './src/scripts/index.js'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: __dirname + '/assets',
+  },
+	plugins: [
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
+	],
+	module: {
+		rules: [{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: 'asset',
+			}
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		],
+	},
+};
+
+module.exports = () => {
+  config.mode = 'production',
+	config.watch = true
+	
+  return config;
+};
