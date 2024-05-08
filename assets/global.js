132645ad22ac481b36f4ce339bc0d4191aabf096
Sam Derbyshire <samderbyshire@sams-mbp.home>
2024-05-08 15:31:12 +0100

    countdown timer made global

--- assets/global.js
+++ assets/global.js
@@ -1391,3 +1391,60 @@ window.UspSlider = {
     }, speed);
   }
 };
+
+/*
+* Do not show the countdown timer if its the weekend
+* day will return int 0-6 (sun-sat), 0 and 6 will represent the weekend
+*/
+
+
+class CountdownTimer extends HTMLElement {
+  constructor() {
+    super();
+
+    const d = new Date();
+    let day = d.getDay();
+
+    if(day !== 0 && day !== 6) {
+      this.initialiseTimer();
+    } else {
+      this.classList.add('hide');
+    }
+  }
+
+  initialiseTimer() {
+    var clientHours = this.getAttribute('data-hours') || 0;
+    var clientMinutes = this.getAttribute('data-minutes') || 0;
+
+
+    // Set the date we're counting down to
+    var countDownDate = new Date().setHours(clientHours); 
+    var countDownDate = new Date(countDownDate).setMinutes(clientMinutes);
+    
+    // Update the count down every 1 second
+    var x = setInterval(() => {
+      // Get today's date and time
+      var now = new Date().getTime();
+    
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      // Display the result in the element with id="demo"
+      this.querySelector("[data-countdown-time]").innerHTML =  "<span>" + hours + "h" + "</span>"
+      + "<span>" +  minutes + "m" + "</span>" + "<span>" + seconds + "s" + "</span>";
+    
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        this.classList.add('hide');
+      }
+    }, 1000);
+  }
+}
+
+customElements.define('countdown-timer', CountdownTimer);
